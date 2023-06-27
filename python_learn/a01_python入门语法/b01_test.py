fab9618ac38b497145ce6dcf34fadea361dcdc97
fo <1745179058@qq.com>
2023-06-27 21:59:23 +0800

    python_learn-第一阶段-第二章-类型转换

--- python_learn/a01_python入门语法/b01_test.py
+++ python_learn/a01_python入门语法/b01_test.py
@@ -6,9 +6,21 @@
 多行注释
 """
 # 使用type()查看数据类型
+"""
 t1 = 1
 t2 = 1.0
 t3 = "lalala"
 print(type(t1))
 print(type(t2))
+print(type(t3))
+"""
+# 数据类型转换
+t1 = float(11)
+print(t1)
+print(type(t1))
+
+t2 = str(11)
+print(type(t2))
+
+t3 = int("11")
 print(type(t3))
