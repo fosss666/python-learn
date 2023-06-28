10c225410ba35c247c674f33f6341a86a4054890
fo <1745179058@qq.com>
2023-06-28 15:27:05 +0800

    python_learn-第一阶段-第四章循环语句-案例2

--- python_learn/a01_python入门语法/b03_循环语句.py
+++ python_learn/a01_python入门语法/b03_循环语句.py
@@ -1,3 +1,5 @@
+import random
+
 # 案例1 求1~100的和
 num = 1
 sum = 0;
@@ -5,3 +7,17 @@ while num <= 100:
     sum += num
     num += 1
 print("1~100的和为：%d" % sum)
+# 案例2 猜数字
+num = random.randint(1, 100)
+count = 1
+while True:
+    i = int(input(f"第{count}次猜："))
+    if i == num:
+        print("猜对啦")
+        break
+    else:
+        if i > num:
+            print("猜大了")
+        else:
+            print("猜小了")
+    count += 1
