ce53b3b45db3b9ed3553556d6929789d31ba83c4
fo <1745179058@qq.com>
2023-06-28 10:10:59 +0800

    python_learn-第一阶段-第二章-字符串操作

--- python_learn/a01_python入门语法/b01_test.py
+++ python_learn/a01_python入门语法/b01_test.py
@@ -32,3 +32,25 @@ print("4/2=", 4 / 2)
 print("2/3=", 2 / 3)
 print("9整除2=", 9 // 2)
 print("4指数2=", 4 ** 2)
+
+# 字符串拼接 字符串不能直接用+和整型等拼接在一起，这点和java不一样
+num = 1
+# 错误写法 print("数字是："+ num)
+print("数字是：", num)
+
+# 字符串格式化-占位符
+name = "张三"
+age = 18
+score = 99.555
+# message = "我叫%s，今年%s岁了，考了%s分" % (name, age, score)
+message = "我叫%s，今年%d岁了，考了%f分" % (name, age, score)
+print(message)
+# 数字的精度控制 m.n  m限制宽度（小于原宽度则不生效） n控制精度（四舍五入）
+message = "我叫%s，今年%5d岁了，考了%.2f分" % (name, age, score)
+print(message)
+# 字符串格式化-快速格式化  字符串前面用f标记
+print(f"我叫{name}，今年{age}岁了，考了{score}分")
+# 对表达式进行格式化
+print("1*1=%s" % (1 * 1))
+print(f"1*2={1 * 2}")
+print("字符串的数据类型：%s" % type(""))
