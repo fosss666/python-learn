3f78d51e50418b0e1dc52bc481b0ad9df5a00b12
fo <1745179058@qq.com>
2023-06-28 14:40:36 +0800

    python_learn-第一阶段-第三章-判断语句-bool&if

---
+++ python_learn/a01_python入门语法/b02_判断语句.py
@@ -0,0 +1,13 @@
+# 布尔类型
+num_1 = 10
+num_2 = 20
+print(num_1 == num_2)
+print(num_1 != num_2)
+print(num_1 > num_2)
+print(f"num_1比num_2大吗？{num_1 > num_2}")
+
+# if语句
+age = 18
+if age >= 18:
+    print(f"我成年了，今年{age}岁喽")
+
