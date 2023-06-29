9c172d3975f04872eaab6d097af0ddbc9674d889
fo <1745179058@qq.com>
2023-06-29 12:07:18 +0800

    python_learn-第一阶段-第六章数据容器-元组

--- python_learn/a01_python入门语法/b05_数据容器.py
+++ python_learn/a01_python入门语法/b05_数据容器.py
@@ -1,24 +1,32 @@
 # 列表 元素类型不受限
-liebiao = ['itheima', 666, True]
-print(liebiao)
-print(type(liebiao))
-arr2 = [[1, 2, 3], [2, 3, 4]]
-print(arr2)
-print(liebiao[1])
-print(arr2[0][0])
+# liebiao = ['itheima', 666, True]
+# print(liebiao)
+# print(type(liebiao))
+# arr2 = [[1, 2, 3], [2, 3, 4]]
+# print(arr2)
+# print(liebiao[1])
+# print(arr2[0][0])
 # 列表的反向索引 -1 -2 -3
-print(liebiao[-2])
+# print(liebiao[-2])
 # 列表常用操作
-l = [21, 25, 21, 23, 22, 20]
-l.append(31)  # 追加数字到列表尾部
-l.extend([29, 33, 30])  # 追加新列表到尾部
-print(l[0])  # 第一个元素
-print(l[-1])  # 最后一个元素
-print(l.index(31))  # 查找元素31的下标
-del l[0]  # 删除第一个元素
-l.pop(-1)  # 删除最后一个元素
-l.remove(25)  # 删除第一个出现的该元素
-print(l.count(1))  # 查看某元素个数
-print(len(l))  # 查看元素总个数
-l.clear()  # 清空列表
-print(l)
+# l = [21, 25, 21, 23, 22, 20]
+# l.append(31)  # 追加数字到列表尾部
+# l.extend([29, 33, 30])  # 追加新列表到尾部
+# print(l[0])  # 第一个元素
+# print(l[-1])  # 最后一个元素
+# print(l.index(31))  # 查找元素31的下标
+# del l[0]  # 删除第一个元素
+# l.pop(-1)  # 删除最后一个元素
+# l.remove(25)  # 删除第一个出现的该元素
+# print(l.count(1))  # 查看某元素个数
+# print(len(l))  # 查看元素总个数
+# l.clear()  # 清空列表
+# print(l)
+
+# 元组 不可被修改
+my_tuple = ('周杰伦', 11, ['football', 'music'])
+print(my_tuple.index(11))
+print(my_tuple[0])
+my_tuple[2].pop(0)
+my_tuple[2].append('coding')
+print(my_tuple)
