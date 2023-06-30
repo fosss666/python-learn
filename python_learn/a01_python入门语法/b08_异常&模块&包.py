4dd80d18cbe5cf106380eb3165828a60760c7fcd
fo <1745179058@qq.com>
2023-06-30 20:45:58 +0800

    python_learn-第一阶段-第九章异常、模块与包-异常传递性

--- python_learn/a01_python入门语法/b08_异常&模块&包.py
+++ python_learn/a01_python入门语法/b08_异常&模块&包.py
@@ -22,4 +22,24 @@
 # finally:
 #     print("有没有异常都要执行")
 
-#
+# 异常的传递
+def f1():
+    print("f1执行")
+    1 / 0
+    print("f1结束")
+
+
+def f2():
+    print("f2执行")
+    f1()
+    print("f2结束")
+
+
+def main():
+    try:
+        f2()
+    except Exception as e:
+        print(f"捕获到异常:{e}")
+
+
+main()
