111049cb0a7d735cd5b81f43a6360d12bf741ecb
fo <1745179058@qq.com>
2023-06-30 13:01:57 +0800

    python_learn-第一阶段-第七章函数拓展-参数传递

---
+++ python_learn/a01_python入门语法/b06_函数拓展.py
@@ -0,0 +1,33 @@
+# 函数参数的传递
+# 1.位置参数
+def f1(name, age, id):
+    print(f"name:{name},age:{age},id:{id}")
+
+
+f1('张三', 18, 222)
+# 2.关键字传递
+f1(name='里斯', age=9, id=22)
+
+
+# 3.默认值
+def f1(name, age, id=0):
+    print(f"name:{name},age:{age},id:{id}")
+
+
+f1('wangwu', 18)
+
+
+# 4.不定长-位置传递
+def f1(*args):
+    print(f"参数类型是：{type(args)},内容为：{args}")
+
+
+f1(12, 3, 4)
+
+
+# 5.不定长-关键字传递
+def f1(**args):
+    print(f"参数类型是：{type(args)},内容为：{args}")
+
+
+f1(name="张三", age=11)
