88f370c130f862ee0f376bc3ae0f102301b497e7
fo <1745179058@qq.com>
2023-06-30 20:37:46 +0800

    python_learn-第一阶段-第九章异常、模块与包-异常捕获

---
+++ python_learn/a01_python入门语法/b08_异常&模块&包.py
@@ -0,0 +1,25 @@
+# 异常
+# try:
+#     1 / 0
+# except:
+#     print("捕获到异常")
+# try:
+#     1 / 0
+# except Exception as e:
+#     print(f"捕获到异常:{e.args}")
+
+# try:
+#     print(name)
+# except (ZeroDivisionError,NameError) as e:
+#     print(f"捕获到异常:{e.args}")
+
+# try:
+#     print("执行：")
+# except Exception as e:
+#     print(f"捕获到异常:{e.args}")
+# else:
+#     print("没有异常")
+# finally:
+#     print("有没有异常都要执行")
+
+#
