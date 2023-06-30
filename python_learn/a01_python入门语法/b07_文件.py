069ccd542b9331430db0be6f2b8d6d9865640504
fo <1745179058@qq.com>
2023-06-30 17:15:08 +0800

    python_learn-第一阶段-第八章文件-写

--- python_learn/a01_python入门语法/b07_文件.py
+++ python_learn/a01_python入门语法/b07_文件.py
@@ -13,15 +13,21 @@
 # print(count)
 # f.close()
 # with open-自动关闭文件
-with open("E:/fo的python学习\python_learn/a01_python入门语法/word.txt", "r", encoding="utf-8") as f:
-    a = f.readlines()
-    print(a)
-    count = 0
+# with open("E:/fo的python学习\python_learn/a01_python入门语法/word.txt", "r", encoding="utf-8") as f:
+#     a = f.readlines()
+#     print(a)
+#     count = 0
+#
+#     for b in a:
+#         c = b.split(" ")
+#         for d in c:
+#             if d == "itheima":
+#                 count += 1
+#
+#     print(count)
 
-    for b in a:
-        c = b.split(" ")
-        for d in c:
-            if d == "itheima":
-                count += 1
-
-    print(count)
+# 写操作,没有该文件则自动创建，每次写的内容会将原文件中的内容替换掉
+f = open("E:/fo的python学习\python_learn/a01_python入门语法/write.txt", "w", encoding="utf-8")
+f.write("Hello World!!")
+# f.flush()
+f.close()
