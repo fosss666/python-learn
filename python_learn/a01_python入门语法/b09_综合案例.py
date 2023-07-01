d5e8ba24463a7c8b66a3497357a9bc22fab408e9
fo <1745179058@qq.com>
2023-07-01 15:05:58 +0800

    python_learn-第一阶段-第十章数据可视化综合案例-pyecharts入门

--- python_learn/a01_python入门语法/b09_综合案例.py
+++ python_learn/a01_python入门语法/b09_综合案例.py
@@ -1,23 +1,39 @@
 # json和python的转换
-import json
+# import json
+#
+# # python转json
+# data = {"name": "张三", "age": 18}
+# print(json.dumps(data, ensure_ascii=False))
+# data = [
+#     {"name": "张三", "age": 18},
+#     {"name": "张三", "age": 18},
+#     {"name": "张三", "age": 18},
+# ]
+# print(json.dumps(data, ensure_ascii=False))
+#
+# # json转python
+# s = '{"name": "张三", "age": 18}'
+# d = json.loads(s)
+# print(type(d))
+# print(d)
+#
+# s = '[{"name": "张三", "age": 18}, {"name": "张三", "age": 18}, {"name": "张三", "age": 18}]'
+# d = json.loads(s)
+# print(type(d))
+# print(d)
+from pyecharts.charts import Line
+from pyecharts.options import TitleOpts, LegendOpts, ToolboxOpts, VisualMapOpts
 
-# python转json
-data = {"name": "张三", "age": 18}
-print(json.dumps(data, ensure_ascii=False))
-data = [
-    {"name": "张三", "age": 18},
-    {"name": "张三", "age": 18},
-    {"name": "张三", "age": 18},
-]
-print(json.dumps(data, ensure_ascii=False))
+line = Line()
+line.add_xaxis(["中国", "美国", "英国"])
+line.add_yaxis("GDP", [300, 200, 100])
 
-# json转python
-s = '{"name": "张三", "age": 18}'
-d = json.loads(s)
-print(type(d))
-print(d)
+# 全局配置项
+line.set_global_opts(
+    title_opts=TitleOpts(title="GDP展示", pos_left="center", pos_bottom="1%"),
+    legend_opts=LegendOpts(is_show=True),
+    toolbox_opts=ToolboxOpts(is_show=True),
+    visualmap_opts=VisualMapOpts(is_show=True)
+)
 
-s = '[{"name": "张三", "age": 18}, {"name": "张三", "age": 18}, {"name": "张三", "age": 18}]'
-d = json.loads(s)
-print(type(d))
-print(d)
+line.render()
