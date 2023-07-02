a00f13f5585ab421aaef2eec8ff0713da559b551
fo <1745179058@qq.com>
2023-07-02 14:55:59 +0800

    python_learn-第一阶段-第十章数据可视化综合案例-基础时间线柱状图

--- python_learn/a01_python入门语法/b09_综合案例.py
+++ python_learn/a01_python入门语法/b09_综合案例.py
@@ -197,8 +197,10 @@ m.render("江苏省各市确诊情况.html")
 """
 
 # =========================柱状图==========================
-from pyecharts.charts import Bar
+from pyecharts.charts import Bar, Timeline
 from pyecharts.options import LabelOpts
+from pyecharts.globals import ThemeType
+
 # 基础柱状图
 """
 bar = Bar()
@@ -209,4 +211,30 @@ bar.add_yaxis("GDP", [30, 20, 10], label_opts=LabelOpts(position="right"))
 bar.reversal_axis()
 
 bar.render("基础柱状图.html")
+"""
+# ================基础时间线柱状图==================
+"""
+bar1 = Bar()
+bar1.add_xaxis(["中国", "美国", "日本"])
+bar1.add_yaxis("GDP", [30, 20, 10], label_opts=LabelOpts(position="right"))
+bar2 = Bar()
+bar2.add_xaxis(["中国", "美国", "日本"])
+bar2.add_yaxis("GDP", [60, 60, 60], label_opts=LabelOpts(position="right"))
+bar3 = Bar()
+bar3.add_xaxis(["中国", "美国", "日本"])
+bar3.add_yaxis("GDP", [80, 50, 10], label_opts=LabelOpts(position="right"))
+
+timeline = Timeline({"theme": ThemeType.LIGHT})  # 设置主题
+timeline.add(bar1, "2010年")
+timeline.add(bar2, "2020年")
+timeline.add(bar3, "2030年")
+
+# 设置自动播放
+timeline.add_schema(
+    is_auto_play=True,  # 是否自动播放
+    is_loop_play=True,  # 是否循环播放
+    play_interval=1000  # 跳转间隔时间1s
+)
+
+timeline.render("基础时间线柱状图.html")
 """
