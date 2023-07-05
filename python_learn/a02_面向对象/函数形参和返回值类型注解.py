1fe7b47d4db2963ca3ac438535437f8ad1ad5eb8
fo <1745179058@qq.com>
2023-07-05 12:26:53 +0800

    python_learn-第二阶段-第一章-函数形参和返回值的类型注解

---
+++ python_learn/a02_面向对象/函数形参和返回值类型注解.py
@@ -0,0 +1,14 @@
+"""    
+    @author: fosss
+    Date: 2023/7/5
+    Time: 12:25
+    Description:
+"""
+
+
+def fun(a: int, b: dict[int, str]) -> bool:
+    print(a, b)
+    return True
+
+
+print(fun(6, {6: "六"}))
