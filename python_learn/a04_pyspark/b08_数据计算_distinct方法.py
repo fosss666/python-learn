b623d6f968c7058076e93d8a29486be9e70e80c8
fo <1745179058@qq.com>
2023-07-08 16:25:08 +0800

    python_learn-第三阶段-数据计算-distinct方法

--- python_learn/a04_pyspark/b08_数据计算_distinct方法.py
+++ python_learn/a04_pyspark/b08_数据计算_distinct方法.py
@@ -1,8 +1,8 @@
 """    
     @author: fosss
     Date: 2023/7/8
-    Time: 16:15
-    Description:
+    Time: 16:23
+    Description:distinct用于去重
 """
 from pyspark import SparkConf, SparkContext
 import os
@@ -11,9 +11,9 @@ os.environ['PYSPARK_PYTHON'] = 'F:/python/python.exe'
 conf = SparkConf().setMaster('local[*]').setAppName('test_spark')
 sc = SparkContext(conf=conf)
 
-rdd = sc.parallelize([1, 2, 3, 4, 5])
+rdd = sc.parallelize([1, 1, 1, 2, 2, 3, 2, 3, 4, 5])
 
-rdd2 = rdd.filter(lambda x: x % 2 == 0)
+rdd2 = rdd.distinct()
 print(rdd2.collect())
 
 sc.stop()
